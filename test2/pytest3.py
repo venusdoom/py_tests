d3e8d82281d327c603b2b79a6bfec8aee7505166
venusdoom <venusdoom91@gmail.com>
2018-06-04 20:38:28 +0300

    initial commit

---
+++ test2/pytest3.py
@@ -0,0 +1,15 @@
+# my first "hello world" program
+# testing "print" function
+
+print("game over")
+print("\ntest new line string arg 1" + " string2 " \
+      + "string3" + "\nnew line string4")
+print("test x5 " * 5)
+print("\a")
+print('800 - 500 + 5 =', 800 - 500 + 5)
+print('800 / 5 =', 800 / 5)
+print('800 // 5 =', 800 // 5)
+print('8 / 5 =', 8 / 5)
+print('8 // 5 =', 8 // 5)
+print('8 % 5 =', 8 % 5)
+input("\nPress Enter to exit")
---
+++ test2/var_tests.py
@@ -0,0 +1,6 @@
+# testing variables
+
+name = input("Your name is: ")
+print(name)
+print("Hello,", name)
+input("\nPress Enter to exit")
